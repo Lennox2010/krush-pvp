c6b151ad94ed058244080266834f8506cf447930
Lennox2010 <125083311+Lennox2010@users.noreply.github.com>
2024-05-20 14:49:31 +0200

    Create updated.lua

---
+++ updated.lua
@@ -0,0 +1,81 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Hydra Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Hydra Hub"})
+
+local Tab = Window:MakeTab({
+    Name = "Aimbot",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Aimbot",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V2/main/Resources/Scripts/Main.lua"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "ESP",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "ESP",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/main/2D%20Box%20ESP/ESP%20%2B%20Health%20Bars.lua"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "Hitbox",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Hitbox",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/hitbox/main/hitbox.lua"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "Spinbot",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Spinbot",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/spinbot/main/spinbot.lua"))()
+      end
+})
+
+
+OrionLib:Init()
