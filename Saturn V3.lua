fb2d259654ecd93af033469f39884a9e9caca42b
Lennox2010 <125083311+Lennox2010@users.noreply.github.com>
2024-05-21 16:33:02 +0200

    Create Saturn V3.lua

---
+++ Saturn V3.lua
@@ -0,0 +1,123 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Saturn X", HidePremium = false, SaveConfig = true, ConfigFolder = "Hydra Hub"})
+
+local Tab = Window:MakeTab({
+    Name = "Aimbot",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Aimbot",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/2d3d/main/23323.lua"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "Silent Aim",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Silent Aim",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/krush-pvp/main/updatedv2.lua"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "ESP",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "ESP",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua',true))()
+      end
+})
+
+
+
+local Tab = Window:MakeTab({
+    Name = "Skeleton ESP",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Skeleton ESP",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/mnynbe/main/ss.lua"))()
+      end
+})
+
+
+
+local Tab = Window:MakeTab({
+    Name = "Hitbox",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Hitbox",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Update-script-hitbox-9326"))()
+      end
+})
+
+local Tab = Window:MakeTab({
+    Name = "Spinbot",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+function MakeScriptHub()
+
+end
+
+Tab:AddButton({
+    Name = "Spinbot",
+    Callback = function()
+              print("button pressed")
+            MakeScriptHub()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lennox2010/spinbot/main/spinbot.lua"))()
+      end
+})
+
+
+OrionLib:Init()
